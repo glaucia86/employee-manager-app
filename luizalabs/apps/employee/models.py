84e82a0fed684817a4ca82ab544e0f1eecd02b7b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-11 16:52:25 -0200

    Gerando o cmd 'makemigrations' e 'migrate' para atualizar as infos dos modelos para a BD.

--- luizalabs/apps/employee/models.py
+++ luizalabs/apps/employee/models.py
@@ -11,6 +11,7 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model): 
+		employee_id = models.CharField(max_length=10, primary_key=True)
 		name = models.CharField(max_length=50)
 		email = models.EmailField(max_length=30)
 		departament = models.CharField(max_length=50)
