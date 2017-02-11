e2cfc668ab29e1f9b0a1bd45d21d5d8afce8a0f0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-11 15:49:04 -0200

    Executando o cmd 'migrate' visnado atualizar as alteraçõs no arquivo 'settings.py.

--- luizalabs/luizalabs/settings.py
+++ luizalabs/luizalabs/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.address',
+    'apps.employee',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
