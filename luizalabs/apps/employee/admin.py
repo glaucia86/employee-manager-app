61cd5a91dbab9943f39e5823ad0697bdcadc4ede
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-12 00:25:33 -0200

    Alteração dos arquivos admin.py de cada modelo criado.

--- luizalabs/apps/employee/admin.py
+++ luizalabs/apps/employee/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.employee.models import Employee
+
 # Register your models here.
+admin.site.register(Employee)
--- luizalabs/apps/personalInfo/admin.py
+++ luizalabs/apps/personalInfo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.personalInfo.models import PersonalInfo
+
 # Register your models here.
+admin.site.register(PersonalInfo)
