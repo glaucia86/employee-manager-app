f2edc4fa0273dfde4437863eb69e77859b52f913
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-12 00:06:40 -0200

    Criação do modelo: 'PersonalInfo'

--- luizalabs/apps/employee/models.py
+++ luizalabs/apps/employee/models.py
@@ -9,10 +9,6 @@
 
 from django.db import models
 
-# Importando o pacote para poder fazer o relacionamento de 1 para 1
-from apps.personalInformation.models import PersonalInformation
-
-
 class Employee(models.Model): 
 		employee_id = models.CharField(max_length=10, primary_key=True)
 		name = models.CharField(max_length=50)
--- luizalabs/apps/personalInfo/models.py
+++ luizalabs/apps/personalInfo/models.py
@@ -1,3 +1,19 @@
+#
+# Arquivo: personalInfo/models.py
+# Author: Glaucia Lemos
+# Data: 11/02/2017
+# 
+# Description: Arquivo responsável por conter todas as informações inerentes ao pacote: 'personalInfo'
+# 
+
 from django.db import models
 
-# Create your models here.
+class PersonalInfo(models.Model): 
+		gender = models.CharField(max_length=10)
+		age = models.IntegerField()
+		birthday = models.DateField()
+		phone = models.CharField(max_length=12)
+		address = models.TextField()
+
+		
+	
