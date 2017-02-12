456d30848fb401e1e29f6d394c196feac79cca89
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-11 23:45:19 -0200

    Acertando o relacionamento entre as classes.

--- luizalabs/apps/personalInformation/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalinformationConfig(AppConfig):
-    name = 'personalInformation'
--- luizalabs/luizalabs/settings.py
+++ luizalabs/luizalabs/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.personalInformation',
-    'apps.employee',
 ]
 
 MIDDLEWARE = [
