1c338f141d26f2f00110a4848c19a3f5673f4e62
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-12 00:15:10 -0200

    Realizando o relacionamento entre as classes (1 para *)

--- luizalabs/apps/employee/models.py
+++ luizalabs/apps/employee/models.py
@@ -9,8 +9,13 @@
 
 from django.db import models
 
+from apps.personalInfo.models import PersonalInfo
+
 class Employee(models.Model): 
 		employee_id = models.CharField(max_length=10, primary_key=True)
 		name = models.CharField(max_length=50)
 		email = models.EmailField(max_length=30)
 		departament = models.CharField(max_length=50)
+
+		# Aqui estou fazendo o relacionando de 1 para * 
+		personalInfo = models.ForeignKey(PersonalInfo, null=True, blank=True, on_delete=models.CASCADE)
