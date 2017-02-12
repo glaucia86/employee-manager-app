4386a64ad1529864a9a39f65235cd296e18632fd
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-12 00:02:50 -0200

    Criação da Classe Modelo: Employee.

--- luizalabs/apps/employee/models.py
+++ luizalabs/apps/employee/models.py
@@ -1,3 +1,20 @@
+
+#
+# Arquivo: employee/models.py
+# Author: Glaucia Lemos
+# Data: 11/02/2017
+# 
+# Description: Arquivo responsável por conter todas as informações inerentes ao pacote: 'employee'
+# 
+
 from django.db import models
 
-# Create your models here.
+# Importando o pacote para poder fazer o relacionamento de 1 para 1
+from apps.personalInformation.models import PersonalInformation
+
+
+class Employee(models.Model): 
+		employee_id = models.CharField(max_length=10, primary_key=True)
+		name = models.CharField(max_length=50)
+		email = models.EmailField(max_length=30)
+		departament = models.CharField(max_length=50)
